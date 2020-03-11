9621579b7717b11336a9f4aa23775fa8577d4859
mizunt1 <mizunt@gmail.com>
2020-03-11 15:43:27 +0000

    restrucutured files

--- models/ard.py
+++ models/ard.py
@@ -27,9 +27,6 @@ def sep_params(matrix, features):
     w = matrix[:features]
     tau = matrix[features+1]
     alpha = matrix[features+1:]
-    print("w issss", w.shape)
-    print("tau isss", tau.shape)
-    print("alpha iss", alpha.shape)
     return w, tau, alpha
 
 def joint_log_prob(y, x, params):
@@ -39,16 +36,11 @@ def joint_log_prob(y, x, params):
     # input current estimates of parameters, data,
     # return log joint distribution
     w, tau, alpha = sep_params(params, features)
-    print("j check1")
     alpha_prior = alpha_prior_()
-    print("j check2")   
     one_over_sqrt_alpha = convert_alpha(alpha)
     tau_prior = tau_prior_()
-    print("j check3")   
     sigma = tf.math.sqrt(tau)
-    print("j check4")   
     w_prior = w_prior_(sigma, one_over_sqrt_alpha)
-    print("j check5")   
     likelihood = tfd.Normal(tf.linalg.matvec(x, w, transpose_a=True), sigma).log_prob(y)
     sum_alpha_prior = tf.reduce_sum(alpha)
     print("join log called")
@@ -57,16 +49,11 @@ def joint_log_prob(y, x, params):
     return sum_is
 
 def return_initial_state(features):
-    print("check 7")
     alpha = alpha_prior_().sample([features])
-    print("alpha is",alpha.shape)
     tau = tau_prior_().sample()
     sigma = tf.math.sqrt(tau)
-    print("tau is", tau)
     tau = tf.reshape(tau, [1])
-    print("sigma", sigma)
     one_over_sqrt_alpha = convert_alpha(alpha)
-    print("check 10")
     w = w_prior_(sigma, one_over_sqrt_alpha).sample()
 
     print("concat shape", tf.concat([w, tau, alpha],0).shape )
