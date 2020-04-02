e6118df7515c116d6b192d987f7a2346843d3b47
Julius Hense <julius.hense@rwth-aachen.de>
2020-04-02 19:32:56 +0200

    Cleaned up de_example.

--- de_example.py
+++ de_example.py
@@ -3,14 +3,14 @@ from models.dirichlet_exponential import DirichletExponential
 from data import frey_face
 from utils.sep_data import sep_training_test
 
-# prepare data for training and testing
-data = frey_face.load_data()
-held_out = 0.2
-train_data, test_data = sep_training_test(data, test_split=held_out, permute=True)
 
-# generate dirichlet exponential model
-model = DirichletExponential(users=28, items=20, factors=10)
+if __name__ == "__main__":
+    # prepare data for training and testing
+    data = frey_face.load_data()
+    train_data, test_data = sep_training_test(data, test_split=0.2, permute=True)
 
-# run advi
-tl.run_train_advi(model, train_data, test_data,
-                  m=1, p=1, skip_steps=10)
+    # generate dirichlet exponential model
+    model = DirichletExponential(users=28, items=20, factors=10)
+
+    # run advi
+    tl.run_train_advi(model, train_data, test_data, m=1, skip_steps=10)
