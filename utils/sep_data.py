58a3520217ea83273243f85bb3532b104b114070
mizunt1 <mizunt@gmail.com>
2020-04-02 09:26:30 +0100

    bug fixed

--- utils/sep_data.py
+++ utils/sep_data.py
@@ -13,9 +13,9 @@ def sep_training_test(x, y=None, num_test=-1, test_split=0.2, permute=False):
           y = np.random.permutation(y)
 
   x_train = x[num_test:,:]
-  x_test = x[num_test:,:]
+  x_test = x[:num_test,:]
   if y is not None:
-      y_train = y[:num_test]
+      y_train = y[num_test:]
       y_test = y[:num_test]
       return (x_train, y_train), (x_test, y_test)
   return x_train, x_test
