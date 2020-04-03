7889e98b3b99358e94a76c0605c3f71325180e06
mizunt1 <mizunt@gmail.com>
2020-04-03 09:16:49 +0100

    train

--- cgp_example.py
+++ cgp_example.py
@@ -5,11 +5,11 @@ from models.constrained_gamma_poisson import Gamma_Poisson
 model = Gamma_Poisson()
 
 # run advi
-#tl.run_train_advi(model, model._train_data, model._test_data, lr=0.001, step_limit=800)
+tl.run_train_advi(model, model._train_data, model._test_data, lr=0.001, step_limit=3000)
 
-#tl.run_train_advi(model, model._train_data, model._test_data, lr=0.001, m=10, step_limit=800)
+tl.run_train_advi(model, model._train_data, model._test_data, lr=0.001, m=10, step_limit=3000)
 
 #run hmc
-tl.run_train_hmc(model, model._train_data, model._test_data, step_size=0.01, num_results=500, num_burnin_steps=100)
+tl.run_train_hmc(model, model._train_data, model._test_data, step_size=0.01, num_results=1000, num_burnin_steps=0)
 
-tl.run_train_nuts(model, model._train_data, model._test_data, step_size=0.01, num_results=500, num_burnin_steps=100)
+tl.run_train_nuts(model, model._train_data, model._test_data, step_size=0.01, num_results=1000, num_burnin_steps=0)
