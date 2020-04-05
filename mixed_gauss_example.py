c45b3cccc568f744a9a471425ff674812b43fd95
julianroth <julianthomasroth@gmail.com>
2020-04-05 13:16:57 +0200

    mixed gauss example

---
+++ mixed_gauss_example.py
@@ -0,0 +1,42 @@
+import train_log as tl
+from models.mixed_gauss import MixedGauss
+from utils.plot import plot, plot_results
+import sys
+
+
+if __name__ == "__main__":
+    # generate hierarchical logistic regression model
+    model = MixedGauss(id_transform=False)
+    arg = str(sys.argv[-1])
+
+    if arg == 'plot':
+        base = 'logs/mixed_gauss/mg_'
+        advi_file = base + 'advi.csv'
+
+        plot_results(advi_file)
+    else:
+        # run advi
+        advi = tl.run_train_advi(model, None, None,
+                          step_limit=-1, m=1, lr=0.1)
+
+        import tensorflow as tf
+        import tensorflow_probability as tfp
+        tfd = tfp.distributions
+
+        num_samples = 10000
+        d1 = tfd.MultivariateNormalFullCovariance(model._mu[0, :], model._std[0, :, :])
+        d2 = tfd.MultivariateNormalFullCovariance(model._mu[1, :], model._std[1, :, :])
+        data = tf.concat([d1.sample(num_samples // 2), d2.sample(num_samples // 2)], 0)
+        print(data.shape)
+        x = data[:, 0]
+        y = data[:, 1]
+        import seaborn as sns
+        import matplotlib.pyplot as plt
+        sns.kdeplot(x.numpy(), y.numpy(), shade=False, shade_lowest=False, cmap='Blues')
+
+        d = tfd.Normal(advi.mu, tf.math.exp(advi.omega))
+        data = d.sample(num_samples)
+        x = data[:, 0]
+        y = data[:, 1]
+        sns.kdeplot(x.numpy(), y.numpy(), shade=False, shade_lowest=False, cmap='Reds')
+        plt.show()
---
+++ models/mixed_gauss.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+import tensorflow_probability as tfp
+tfd = tfp.distributions
+
+
+class MixedGauss:
+
+    def __init__(self, id_transform=True):
+        self._mu = tf.constant([[-1, -1], [1, 1]], dtype=tf.float64)
+        self._std = tf.constant([[[.5, .25], [.25, 1.]],
+                                 [[.5, -.25], [-.25, 1.]]], dtype=tf.float64)
+        self.num_params = 2
+        self._id_transform = id_transform
+
+    def log_likelihood(self, data, params):
+        d1 = tfd.MultivariateNormalFullCovariance(self._mu[0, :], self._std[0, :, :])
+        d2 = tfd.MultivariateNormalFullCovariance(self._mu[1, :], self._std[1, :, :])
+        return tf.math.log(.5 * d1.prob(params) + .5 * d2.prob(params))
+
+    def avg_log_likelihood(self, data, params):
+        return self.log_likelihood(data, params)
+
+    def joint_log_prob(self, data, params):
+        return self.log_likelihood(data, params)
+
+    def sep_params(self, params):
+        return params
+
+    def return_initial_state(self, random=False):
+        pass
+
+    def bijector(self):
+        tfb = tfp.bijectors
+        if self._id_transform:
+            return tfb.Identity()
+        else:
+            return tfb.Shift(tf.constant(2.5, dtype=tf.float64))
