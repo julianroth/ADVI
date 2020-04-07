c9393c0bfb80f0f5b55aa7a0ceba366eb9e248fc
mizunt1 <mizunt@gmail.com>
2020-04-07 10:30:37 +0100

    hmc now will run in constrained

--- cgp_example.py
+++ cgp_example.py
@@ -2,14 +2,14 @@ import train_log as tl
 from models.constrained_gamma_poisson import Gamma_Poisson
 
 # generate dirichlet exponential model
-model = Gamma_Poisson(transform=True)
+model = Gamma_Poisson(transform=False)
 
 # run advi
-#tl.run_train_advi(model, model._train_data, model._test_data, lr=0.1, step_limit=8000)
+#tl.run_train_advi(model, model._train_data, model._test_data, lr=0.1, step_limit=18000)
 # paper saids they used lr = 0.1
-#tl.run_train_advi(model, model._train_data, model._test_data, lr=0.1, m=10, step_limit=8000)
-
+#tl.run_train_advi(model, model._train_data, model._test_data, lr=0.1, m=10, step_limit=13000)
+model = Gamma_Poisson(transform=True)
 #run hmc
-tl.run_train_hmc(model, model._train_data, model._test_data, step_size=0.001, num_results=3500, num_burnin_steps=0)
+#tl.run_train_hmc(model, model._train_data, model._test_data, step_size=0.001, num_results=5500, num_burnin_steps=0)
 
-tl.run_train_nuts(model, model._train_data, model._test_data, step_size=0.001, num_results=3500, num_burnin_steps=0)
+tl.run_train_nuts(model, model._train_data, model._test_data, step_size=0.001, num_results=5500, num_burnin_steps=0)
