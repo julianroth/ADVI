8638ddd992dffa5c68fe012c5ecd51cc23bf32a0
mizunt1 <mizunt@gmail.com>
2020-04-12 22:27:41 +0100

    put model runs back to original

--- ard_example.py
+++ ard_example.py
@@ -16,10 +16,10 @@ def make_training_data(num_samples, dims, sigma, mu=0):
   """
   x = np.random.randn(dims, num_samples).astype(np.float64)
   w = sigma * np.random.randn(1, dims).astype(np.float64)
-  #noise = np.random.randn(num_samples).astype(np.float64)
-  noise = 0.
+  noise = np.random.randn(num_samples).astype(np.float64)
+  noise = 0
   w[:,:int(dims/2)] = 0.
-  y = w.dot(x) + (noise/4) + mu
+  y = w.dot(x) + (noise/2) + mu
     
   return y, x, w
 
@@ -34,13 +34,8 @@ def sep_training_test(y,x,test):
 
 num_features = 250
 
-y, x, w = make_training_data(10000, num_features, 0.3, mu=0)
+y, x, w = make_training_data(10000, num_features,10, mu=0)
 y_train, y_test, x_train, x_test = sep_training_test(y,x,1000)
-
-#print(y_train.shape)
-#print(y_test.shape)
-#print(x_train.shape)
-#print(x_test.shape)
 
 train_data = (y_train, x_train)
 test_data = (y_test, x_test)
@@ -50,14 +45,11 @@ test_data = (y_test, x_test)
 model = Ard(num_features=num_features, transform=False)
 
 # run advi
-#tl.run_train_advi(model, train_data, test_data, step_limit=15000, lr=0.1)
+tl.run_train_advi(model, train_data, test_data, step_limit=1000, lr=0.1, adam=True)
 # paper saids they used lr = 0.1
-#tl.run_train_advi(model, train_data, test_data, step_limit=5000, lr=0.1, m=10)
+tl.run_train_advi(model, train_data, test_data, step_limit=5000, lr=0.1, adam=True, m=10)
 model = Ard(num_features=num_features, transform=True)
 
-
-tl.run_train_hmc(model, train_data, test_data, step_size=0.001, num_results=2000, num_burnin_steps=50)
-# run hmc
-
+tl.run_train_hmc(model, train_data, test_data, step_size=0.1, num_results=1000)
 
 tl.run_train_nuts(model, train_data, test_data, step_size=0.001, num_results=1000)
